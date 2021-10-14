abc86593c91b353567868c213f60a6ee94942475
Việt Anh Nguyễn <[emdemboy69@gmail.com]>
2021-10-14 21:33:14 +0700

    [all file]

---
+++ demo2.cpp
@@ -0,0 +1,16 @@
+##include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a;
+   int b;
+   int tong;
+   cout << "nhap a: "; 
+   cin >> a ;
+   cout << "nhap b: ";
+   cin >> b ;
+   tong = a + b;
+   cout << "tong = " << tong << endl;
+}
